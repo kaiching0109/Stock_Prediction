44c8217d04b7631acdfedaa679c4d17fb4af9659
kaiching0109 <kaiching0109@gmail.com>
2019-04-26 19:11:02 -0400

    UPDATED: added train&test data and able to predict with regression

--- src/models/Kai/prediction_controller.py
+++ src/models/Kai/prediction_controller.py
@@ -1,3 +1,61 @@
 """
 Here is to make predictions.
 """
+
+"""
+SET UP RELATIVE PATH
+"""
+import sys
+import os
+absFilePath = os.path.abspath(__file__)
+fileDir = os.path.dirname(os.path.abspath(__file__))
+rootDir = os.path.dirname(os.path.dirname(fileDir))
+feature_path = os.path.join(rootDir, 'features')
+vizualizer_path = os.path.join(rootDir, 'visualization')
+# data_path = os.path.join(rootDir, 'data')
+sys.path.extend([feature_path, vizualizer_path])
+
+from train_controller import train_controller
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, linear_model
+from feature_builder import raw_data_processor, feature_scaling
+from vizualizer import visualizePredictResult
+from sklearn.metrics import mean_squared_error, r2_score
+
+class prediction_controller:
+    def __init__(self, data_set:tuple):
+        self.X_train, self.X_test, self.Y_train, self.Y_test = data_set
+        self.result = {}
+
+    def predict_with_regression(self):
+        print("MESSAGE: Predicting with regression")
+        # Create linear regression object
+        regr = linear_model.LinearRegression()
+        # Train the model using the training sets
+        print(self.X_train)
+        print(self.Y_train)
+        regr.fit(self.X_train, self.Y_train)
+        # Make predictions using the testing set
+        Y_pred = regr.predict(self.X_test)
+        self.result["regression"] = {
+            "coefficients": regr.coef_,
+            "mean_squared_error": mean_squared_error(self.Y_test, Y_pred),
+            "r-squared": r2_score(self.Y_test, Y_pred),
+            "y_pred": Y_pred
+        }
+
+    def get_result(self):
+        return self.result
+
+if __name__ == '__main__':
+    df = raw_data_processor()
+    trainController = train_controller(df)
+    data_set = trainController.get_processed_data_set()
+    predictionController = prediction_controller(data_set)
+    predictionController.predict_with_regression()
+    result = predictionController.get_result()
+    print(result)
+    X_train, X_test, Y_train, Y_test = data_set
+    Y_pred = result["regression"]["y_pred"]
+    visualizePredictResult(X_test, Y_test, Y_pred)
--- src/models/Kai/train_controller.py
+++ src/models/Kai/train_controller.py
@@ -15,19 +15,43 @@ feature_path = os.path.join(rootDir, 'features')
 sys.path.extend([feature_path])
 
 import pandas as pd
+import numpy as np
+from feature_builder import raw_data_processor, feature_scaling
+from sklearn.model_selection import train_test_split
 
 class train_controller:
     def __init__(self, df: pd.DataFrame):
-        print("train_controller")
-        self.processed_data = df
+        self.processed_data = df.values
+        self.initialize_data()
 
     def initialize_data(self):
-        size = (self.processed_data.shape)[0]
-        train_size = round(size * 0.80)
-        self.train_data = self.processed_data[:train_size]
-        self.test_data = self.processed_data[train_size:]
+        print("MESSAGE: Initializing training and test data...")
+        SPY_CLOSE_COLUMN_NAME = "spy_close_price"
+        SIGNAL_COLUMN_NAME = "signal"
+        try:
+            n_samples, n_features = self.processed_data.shape
+            X = self.processed_data[:, np.newaxis, 1]
+            Y = self.processed_data[:, np.newaxis, 2]
+            # print(Y)
+            # train_size = round(size * 0.80)
+            # self.train_data = self.processed_data[:train_size]
+            # self.test_data = self.processed_data[train_size:]
+            # train_set_scaled, test_set_scaled = feature_scaling(self.train_data, self.test_data)
+
+            # X = self.processed_data[SIGNAL_COLUMN_NAME]
+            # Y = self.processed_data[SPY_CLOSE_COLUMN_NAME]
+            self.X_train, self.X_test, self.Y_train, self.Y_test = train_test_split(
+                                                X, Y, test_size=.2, random_state=0)
+            print("SUCCESS: Initialized training and test data")
+        except Exception as e:
+            print("FAIL: Unable to initialize training and test data")
+            print(e)
 
 
+
+    def get_processed_data_set(self):
+        return self.X_train, self.X_test, self.Y_train, self.Y_test
 
 if __name__ == '__main__':
-    controller = train_controller()
+    df = raw_data_processor()
+    controller = train_controller(df)
